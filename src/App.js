d421f0b35fb685fa0ce233b52b799127a3ec430c
STPekdemir <119021180+STPekdemir@users.noreply.github.com>
2023-01-15 15:24:37 +0300

    API dan data çekildi
    axios usestate useeffect import edildi APİ dan veri çekildi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [apiData, setApiData] = useState(null);
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then((response) => {
+        setApiData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  console.log(apiData);
   return (
     <div className="App">
       <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
+        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip
+        edin İyi eğlenceler!{" "}
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+        !
       </p>
     </div>
   );
